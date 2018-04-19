0f225468a933be4cdf6bc19720b3c2ade8513750
ClementRb <clement.ribourt@gmail.com>
2018-04-19 13:00:01 +0200

    marvel

--- marvel.js
+++ marvel.js
@@ -1,5 +1,13 @@
 var api = require('marvel-api');
+const program = require('commander')
+
+program
+ .version('1.0.0')
+ .option('-f, --femme','Affiche toutes les femmes  Marvel')
+ .option()
  
+program.parse(process.argv)
+
 var marvel = api.createClient({
   publicKey: '8854539c35428148dd3839f93f98598b'
 , privateKey: '45b550549ea76bb81e5de001b53f4ea566cfd346'
@@ -7,7 +15,8 @@ var marvel = api.createClient({
 
 let tabName = {}
 
-marvel.characters.findAll(100)
+if(program.femme){
+ marvel.characters.findAll(100)
   .then(( char ) => {
     for (item of char.data){
         console.log(item.name)
@@ -16,3 +25,6 @@ marvel.characters.findAll(100)
        ) 
   .fail(console.error)
   .done();
+}else if(program.){
+  marvel.characters.findByName('black widow')
+         }
